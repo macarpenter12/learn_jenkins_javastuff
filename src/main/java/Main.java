6f4762beab76f60e590462ac1fc09f42dd22dbc2
Mark Carpenter <mcarpe12@byu.edu>
2020-10-17 16:04:12 -0600

    using if instead of switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,61 +9,47 @@ public class Main {
       System.out.println("Input a command:");
       String command = input.next();
 
-      switch (command) {
-        case "add": {
-          int a = input.nextInt();
-          int b = input.nextInt();
-          input.nextLine();
-          System.out.println(calc.add(a, b));
-          System.out.println();
-          break;
-        }
-        case "subtract": {
-          int a = input.nextInt();
-          int b = input.nextInt();
-          input.nextLine();
-          System.out.println(calc.subtract(a, b));
-          System.out.println();
-          break;
-        }
-        case "multiply": {
-          int a = input.nextInt();
-          int b = input.nextInt();
-          input.nextLine();
-          System.out.println(calc.multiply(a, b));
-          System.out.println();
-          break;
-        }
-        case "divide": {
-          int a = input.nextInt();
-          int b = input.nextInt();
-          input.nextLine();
-          System.out.println(calc.divide(a, b));
-          System.out.println();
-          break;
-        }
-        case "fibonacci": {
-          int n = input.nextInt();
-          input.nextLine();
-          System.out.println(calc.fibonacciNumberFinder(n));
-          System.out.println();
-          break;
-        }
-        case "binary": {
-          int n = input.nextInt();
-          input.nextLine();
-          System.out.println(calc.intToBinaryNumber(n));
-          System.out.println();
-          break;
-        }
-        case "quit":
-          input.close();
-          System.out.println();
-          return;
-        default:
-          System.out.println("Usage: command [args]...");
-          System.out.println();
-          break;
+      if ("add".equals(command)) {
+        int a = input.nextInt();
+        int b = input.nextInt();
+        input.nextLine();
+        System.out.println(calc.add(a, b));
+        System.out.println();
+      } else if ("subtract".equals(command)) {
+        int a = input.nextInt();
+        int b = input.nextInt();
+        input.nextLine();
+        System.out.println(calc.subtract(a, b));
+        System.out.println();
+      } else if ("multiply".equals(command)) {
+        int a = input.nextInt();
+        int b = input.nextInt();
+        input.nextLine();
+        System.out.println(calc.multiply(a, b));
+        System.out.println();
+      } else if ("divide".equals(command)) {
+        int a = input.nextInt();
+        int b = input.nextInt();
+        input.nextLine();
+        System.out.println(calc.divide(a, b));
+        System.out.println();
+      } else if ("fibonacci".equals(command)) {
+        int n = input.nextInt();
+        input.nextLine();
+        System.out.println(calc.fibonacciNumberFinder(n));
+        System.out.println();
+      } else if ("binary".equals(command)) {
+        int n = input.nextInt();
+        input.nextLine();
+        System.out.println(calc.intToBinaryNumber(n));
+        System.out.println();
+      } else if ("quit".equals(command)) {
+        input.close();
+        System.out.println();
+        return;
+      } else {
+        System.out.println("Usage: command [args]...");
+        System.out.println();
       }
     }
   }
